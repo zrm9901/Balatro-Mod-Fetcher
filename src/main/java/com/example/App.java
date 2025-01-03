ca01ab587ae151b34d810f531c4425d8463cd49b
zrm9901 <zrm9901@gmail.com>
2025-01-03 16:02:58 -0500

    Redo code again

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -99,78 +99,92 @@ class App {
             return read();
         }
     }
-    public static boolean checkPath(String steamApps) {
+    public static String checkPath(ArrayList<String> paths) {
+        String returns;
         try {
-            String path = steamApps + "/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro/Mods";
-            String directory = steamApps + "/common/Balatro";
-            File check = new File(path);
-            System.out.println(check);
-            File direct = new File(directory);        
-            if (!check.exists() && direct.exists()) {
-                modsFolder = path;
-                System.out.println(check.exists());
-                System.out.println(direct.exists());
-                System.out.println("Mods folder not detected, fetching lovely");
-                String apiUrl = ("https://api.github.com/repos/ethangreen-dev/lovely-injector/releases/tags/" + getLatestReleaseTag());
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder().url(apiUrl).build();
-                try (Response response = client.newCall(request).execute()) {
-                    if (response.isSuccessful() && response.body() != null) {
-                        String responseBody = response.body().string();
-                        JSONObject release = new JSONObject(responseBody);
-                        JSONArray assets = release.getJSONArray("assets");
-                        if (assets.length() > 0) {
-                            for (int i = 0; i < assets.length(); i++) {
-                                try {
-                                    JSONObject asset = assets.getJSONObject(i);
-                                    String assetName = asset.getString("name");
-                                    String assetDownloadUrl = asset.getString("browser_download_url");;
-                                    
-                                    downloadFile(assetDownloadUrl, directory + File.separator + assetName);
-                                    System.out.println("Download complete: " + assetName);
-                                    String zipFilePath = (directory + File.separator + assetName);
-                                    try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath))) {
-                                        ZipEntry zipEntry;
-                                        String fileName = "version.dll";
-                                        // Iterate through each entry in the ZIP file
-                                        while ((zipEntry = zis.getNextEntry()) != null) {
-                                            if (zipEntry.getName().equals(fileName)) {
-                                                File outputFile = new File(directory, fileName);
-                                                // Ensure parent directories exist
-                                                outputFile.getParentFile().mkdirs();
-                            
-                                                try (FileOutputStream fos = new FileOutputStream(outputFile)) {
-                                                    byte[] buffer = new byte[1024];
-                                                    int length;
-                                                    while ((length = zis.read(buffer)) > 0) {
-                                                        fos.write(buffer, 0, length);
+            for (String steamApps : paths) {
+                String path = steamApps + "/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro/Mods";
+                String directory = steamApps + "/common/Balatro";
+                System.out.println(path);
+                System.out.println(directory);
+                File check = new File(path);
+                
+                File direct = new File(directory);        
+                if (!check.exists() && direct.exists()) {
+                    returns = steamApps;
+                    System.out.println(check.exists());
+                    System.out.println(direct.exists());
+                    System.out.println("Mods folder not detected, fetching lovely");
+                    String apiUrl = ("https://api.github.com/repos/ethangreen-dev/lovely-injector/releases/tags/" + getLatestReleaseTag());
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url(apiUrl).build();
+                    try (Response response = client.newCall(request).execute()) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            String responseBody = response.body().string();
+                            JSONObject release = new JSONObject(responseBody);
+                            JSONArray assets = release.getJSONArray("assets");
+                            if (assets.length() > 0) {
+                                for (int i = 0; i < assets.length(); i++) {
+                                    try {
+                                        JSONObject asset = assets.getJSONObject(i);
+                                        String assetName = asset.getString("name");
+                                        String assetDownloadUrl = asset.getString("browser_download_url");;
+                                        
+                                        downloadFile(assetDownloadUrl, directory + File.separator + assetName);
+                                        System.out.println("Download complete: " + assetName);
+                                        String zipFilePath = (directory + File.separator + assetName);
+                                        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath))) {
+                                            ZipEntry zipEntry;
+                                            String fileName = "version.dll";
+                                            // Iterate through each entry in the ZIP file
+                                            while ((zipEntry = zis.getNextEntry()) != null) {
+                                                if (zipEntry.getName().equals(fileName)) {
+                                                    File outputFile = new File(directory, fileName);
+                                                    // Ensure parent directories exist
+                                                    outputFile.getParentFile().mkdirs();
+                                
+                                                    try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+                                                        byte[] buffer = new byte[1024];
+                                                        int length;
+                                                        while ((length = zis.read(buffer)) > 0) {
+                                                            fos.write(buffer, 0, length);
+                                                        }
                                                     }
+                                                    System.out.println("File extracted to: " + outputFile.getAbsolutePath());
+                                                    
                                                 }
-                                                System.out.println("File extracted to: " + outputFile.getAbsolutePath());
-                                                return false;
                                             }
                                         }
-                                    }
-                                } catch (Exception e) {
-                                    e.getStackTrace();
-                                    return false;
+                                    } catch (Exception e) {
+                                        e.getStackTrace();
+                                        returns = null;
+                                        return returns;
+                                }
+                            }} else {
+                                System.out.println("no assets found");
+                                returns = null;
+                                return returns;
                             }
-                        }} else {
-                            System.out.println("no assets found");
+                        } else {
+                            System.out.println("cant fetch release");
+                            returns = null;
+                            return returns;
                         }
-                    } else {
-                        System.out.println("cant fetch release");
                     }
+                    return returns;
+                } else if (check.exists() && direct.exists()) {
+                    returns = steamApps;
+                    System.out.println("returns");
+                    return returns;
                 }
-                return false;
-            }  else {
-                return true;
             }
         } catch (Exception e) {
             System.out.println("Error fetching lovely" );
             e.printStackTrace();
-            return false;
         }
+        System.out.println("no valid paths");
+        returns = null;
+        return returns;
     }
     public static void downloadFile(String fileUrl, String destinationPath) throws IOException {
         try {
@@ -211,18 +225,19 @@ class App {
         ArrayList<String> mods = read();
         System.out.println("Running as user: " + System.getProperty("user.name"));
         if (!mods.isEmpty()) {
-            if (!checkPath(System.getProperty("user.home") + "/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps")) {
-                System.out.println("If your os is supported, lovely has been downloaded into your Balatro folder. If you are unsure if your system is supported, check in \"Steam>Balatro>Properties>Browse Local Files\" and loook for version.dll. If you dont see it, open an issue on github with your OS, Store, and Mods and Game path. Make sure you open your game again to create the mods folder to clone into. Also, Please add \"WINEDLLOVERRIDES=\"version=n,b\"%command%\"  to you rsteam lauch options. Best of luck modding, zrm9901!");
-            } else if (modsFolder == null){
-                System.out.println("could not find mods folder or game directory folder. make an issue on github to add it. For now please manually input the path to your steamApps folder\nFor example: /home/zrm9901/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps");
-                Scanner sc = new Scanner(System.in);
-                String directory = sc.nextLine();
-                if (!checkPath(directory)) {
-                    System.out.println("If your os is supported, lovely has been downloaded into your Balatro folder. If you are unsure if your system is supported, check in \"Steam>Balatro>Properties>Browse Local Files\" and loook for version.dll. If you dont see it, open an issue on github with your OS, Store, and Mods and Game path. Make sure you open your game again to create the mods folder to clone into. Also, Please add \"WINEDLLOVERRIDES=\"version=n,b\"%command%\"  to you rsteam lauch options. Best of luck modding, zrm9901!");
-                }
-                modsFolder = directory + "/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro/Mods";
-                sc.close();
+            ArrayList<String> steamApps = new ArrayList<>();
+            Scanner sc = new Scanner(System.in);
+            String directory = sc.nextLine();
+            if (!directory.isEmpty()) {
+                steamApps.add(directory);
+                System.out.println("hi");
+            }
+            steamApps.add("/home/zrm9901/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps");
+            sc.close();
+            if (checkPath(steamApps) != null) {
                 System.out.println("cloning now");
+                modsFolder = checkPath(steamApps);
+                System.out.println(modsFolder);
                 if (mods.size() > 0) {
                     for (int i = 0; i < mods.size(); i++) {
                         MyThread thread = app.new MyThread(mods.get(i));
